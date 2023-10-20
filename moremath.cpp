9144312c00bbe068e247e7d75c71b153e2065db7
Martin Williams <martinwork@users.noreply.github.com>
2023-10-20 13:31:26 +0100

    Create moremath.cpp

---
+++ moremath.cpp
@@ -0,0 +1,14 @@
+#include "pxt.h"
+
+//% weight=100 color=#0fbc11 icon=""
+namespace moreMath {
+
+/**
+  * cpow: x raised to the power y
+  */
+//% block="%x pow %y"
+//%
+void cpow( double x, double y) {
+  return pow(x,y); 
+}
+}
